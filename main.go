defe42bbc1fcc2bb92f14f16f8c27560c392e017
Amolith <amolith@secluded.site>
2021-12-25 08:49:35 -0500

    Finish function for parsing the ring list

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"os"
+	"io/ioutil"
+	"log"
 	"strings"
 
 	flag "github.com/spf13/pflag"
 )
 
 type ring struct {
-	name string
-	url  string
+	handle string
+	url    string
 }
 
 // Pre-define all of our flags
@@ -25,16 +26,24 @@ var (
 
 func main() {
 	flag.Parse()
-	list()
+	ring := parseList()
+	fmt.Println(ring)
 }
 
-// List parses the list of members and appends data to the `ring` struct
-func list() {
-	bytes, _ := os.ReadFile(os.Args[1])
-	file := strings.Split(string(bytes), "\n")
-	for _, line := range file {
-		fmt.Println(line)
+// List parses the list of members, appends the data to a slice of type list,
+// then returns the slice
+func parseList() []ring {
+	file, err := ioutil.ReadFile(*flagList)
+	if err != nil {
+		log.Fatal("Error while loading list of webring members:", err)
 	}
+	lines := strings.Split(string(file), "\n")
+	var r []ring
+	for _, line := range lines[:len(lines)-1] {
+		fields := strings.Fields(line)
+		r = append(r, ring{handle: fields[0], url: fields[1]})
+	}
+	return r
 }
 
 // createPage create the root webpage that will be served. It takes a []ring
