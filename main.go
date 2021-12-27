eafc2ea723273bed48ad727cdfc38c1deec03981
Amolith <amolith@secluded.site>
2021-12-26 20:30:09 -0500

    Caveat custom cert/flag comment

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 	flagListen  *string = flag.StringP("listen", "l", "127.0.0.1:2857", "Host and port go-webring will listen on")
 	flagMembers *string = flag.StringP("members", "m", "list.txt", "Path to list of webring members")
 	flagIndex   *string = flag.StringP("index", "i", "index.html", "Path to home page template")
-	// These are not yet implemented
+	// These are not (and might never be) implemented, we'll see
 	// flagCert  *string = flag.StringP("cert", "c", "cert.crt", "Path to certificate")
 	// flagKey   *string = flag.StringP("key", "k", "cert.key", "Path to private certificate key")
 )
