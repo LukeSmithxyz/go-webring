968b6e5057111e268b098676fa6a4ad8d14caae5
Amolith <amolith@secluded.site>
2021-12-26 20:16:51 -0500

    Implement next, previous, and random paths

--- handlers.go
+++ handlers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 // Serves the webpage created by createRoot()
@@ -28,13 +30,51 @@ func root(writer http.ResponseWriter, request *http.Request) {
 // Redirects the visitor to the next member, wrapping around the list if the
 // next would be out-of-bounds
 func next(writer http.ResponseWriter, request *http.Request) {
+	host := request.URL.Query().Get("host")
+	dest := "https://"
+	list := parseList()
+	for i, item := range list {
+		if item.url == host {
+			if i+1 >= len(list) {
+				dest = dest + list[0].url
+				http.Redirect(writer, request, dest, 302)
+				break
+			}
+			dest = dest + list[i+1].url
+			http.Redirect(writer, request, dest, 302)
+			break
+		}
+	}
+	// TODO: Print output to the page telling the visitor that the member
+	// couldn't be found
 }
 
 // Redirects the visitor to the previous member, wrapping around the list of the
 // next would be out-of-bounds
 func previous(writer http.ResponseWriter, request *http.Request) {
+	host := request.URL.Query().Get("host")
+	dest := "https://"
+	list := parseList()
+	for i, item := range list {
+		if item.url == host {
+			if i-1 < 0 {
+				dest = dest + list[len(list)-1].url
+				http.Redirect(writer, request, dest, 302)
+				break
+			}
+			dest = dest + list[i-1].url
+			http.Redirect(writer, request, dest, 302)
+			break
+		}
+	}
+	// TODO: Print output to the page telling the visitor that the member
+	// couldn't be found
 }
 
 // Redirects the visitor to a random member
 func random(writer http.ResponseWriter, request *http.Request) {
+	rand.Seed(time.Now().Unix())
+	list := parseList()
+	dest := "https://" + list[rand.Intn(len(list)-1)].url
+	http.Redirect(writer, request, dest, 302)
 }
