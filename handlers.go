4487849573a21e76c42f4cfb90435d78779d07dc
Amolith <amolith@secluded.site>
2021-12-26 19:38:33 -0500

    Move table headers to user-created HTML

--- handlers.go
+++ handlers.go
@@ -9,18 +9,13 @@ import (
 // Serves the webpage created by createRoot()
 func root(writer http.ResponseWriter, request *http.Request) {
 	r := parseList()
-	table := "\n" + `<table>
-  <tr>
-    <th>Fedi handle</th>
-    <th>Site URL</th>
-  </tr>` + "\n"
+	var table string
 	for _, member := range r {
 		table = table + "  <tr>\n"
 		table = table + "    <td>" + member.handle + "</td>\n"
 		table = table + "    <td>" + link(member.url) + "</td>\n"
 		table = table + "  </tr>\n"
 	}
-	table = table + "</table>\n"
 
 	tmpl, err := template.ParseFiles(*flagIndex)
 	if err != nil {
